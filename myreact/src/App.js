6e1dce8d6bcbade719b145df5dfdadd6c2ef04c2
VitaliyM <vetalmamchur@gmail.com>
2023-01-14 16:43:10 +0000

    2

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import Footer from "./Components/footer/Footer";
 import Mainblock from "./Components/mainblock/Mainblock";
 import Navbar from "./Components/navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import './main.css';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       
-      <Navbar />
-      <Mainblock />
-      <Footer />
+        <Navbar />
+        <Mainblock />
+        <Footer />
 
+      </BrowserRouter>
     </div>
   );
 }
--- myreact/src/Components/mainblock/Mainblock.jsx
+++ myreact/src/Components/mainblock/Mainblock.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './mainblock.css';
+import Contacts from './pages/contacts/Contacts';
+import Home from './pages/home/Home';
+import Projects from './pages/projects/Projects';
+import { Routes, Route } from 'react-router-dom';
 
 function Mainblock() {
   return (
     <main className='main-block'>
-        PAGES
+
+      
+
+        <Home />
+        <Projects />
+        <Contacts />
     </main>
   )
 }
