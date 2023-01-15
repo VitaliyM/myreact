67ebf5c626e7d23928f89b0c0ef6d606ada05d63
VitaliyM <vetalmamchur@gmail.com>
2023-01-15 22:42:44 +0000

    3

--- myreact/src/Components/mainblock/pages/home/Home.jsx
+++ myreact/src/Components/mainblock/pages/home/Home.jsx
@@ -5,27 +5,27 @@ function Home() {
   return (
     <div className='home-block'>
 
-      <section className="mainBlockSection1">
-        <div className="mainBlockText">
+      <section className="home-block__section1">
+        <div className="home-block__text">
           <h1>Hi, my name is Vitalii</h1>
           <h3>a frontend developer</h3>
           <h5>with passion for learning and creating</h5>
-          <button className="downloadCv">Download CV</button>
+          <button className="home-block__download-cv">Download CV</button>
 
-          <div className="rectangles">
-            <div className="rec rect1"></div>
-            <div className="rec rect2"></div>
-            <div className="rec rect3"></div>
-            <div className="rec rect4"></div>
-            <div className="rec rect5"></div>
+          <div className="rectangles-block">
+            <div className="rectangles-block__rec rectangles-block__rect1"></div>
+            <div className="rectangles-block__rec rectangles-block__rect2"></div>
+            <div className="rectangles-block__rec rectangles-block__rect3"></div>
+            <div className="rectangles-block__rec rectangles-block__rect4"></div>
+            <div className="rectangles-block__rec rectangles-block__rect5"></div>
           </div>
 
         </div>
       </section>
 
-      <section className="mainBlockSection2">
-        <div className="skillsHeader">Frontend</div>
-        <div className="skillsText">HTML, CSS, JavaScript, BootStrap, NPM, ReactJS, Redux</div>
+      <section className="home-block__section2">
+        <div className="home-block__skills-header">Frontend</div>
+        <div className="home-block__skills-text">HTML, CSS, JavaScript, BootStrap, NPM, ReactJS, Redux</div>
       </section>
 
     </div>
