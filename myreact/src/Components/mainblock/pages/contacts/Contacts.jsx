59cbe79e100853b8557f775afea5e44ddc883fb1
VitaliyM <vetalmamchur@gmail.com>
2023-01-18 09:28:53 +0000

    5

--- myreact/src/Components/mainblock/pages/contacts/Contacts.jsx
+++ myreact/src/Components/mainblock/pages/contacts/Contacts.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './contacts.css';
 
 function Contacts() {
   return (
-    <div>Contacts</div>
+    <div className='contacts-block'>
+      <h2 className='contacts-block__header'>Contacts</h2>
+      <div className='contacts-block__location'>
+        <h3 className='location__header'>Location</h3>
+        <div className='location__text'>Zhytomyr, Ukraine</div>
+      </div>
+      <div className='contacts-block__messager'>
+        <h3 className='messager__header'>Viber / Telegram</h3>
+        <div className='messager__text'>+380660000000</div>
+      </div>
+      <div className='contacts-block__email'>
+        <h3 className='email__header'>Email</h3>
+        <div className='email__text'>frontend@gmail.com</div>
+      </div>
+    </div>
   )
 }
 
