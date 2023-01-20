37dad7cd8ec42b939f281e294f221d677880323d
VitaliyM <vetalmamchur@gmail.com>
2023-01-20 13:55:29 +0000

    6

--- myreact/src/Components/footer/Footer.jsx
+++ myreact/src/Components/footer/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './footer.css';
 
-function Footer({footerDataList}) {
+function Footer({ footerDataList }) {
   return (
-    <footer className='footer-block'>Footer
+    <footer className='footer-block'>
       <div className='footer-block__images'>
-      {footerDataList.map( (footerData) => {
-        return (
-          <img className='footer-block__img' src={footerData.imgsource} alt={footerData.alt} />
-        )
-      } )}
-      </div>       
-    
+        {footerDataList.map((footerData) => {
+          return (
+            <img className='footer-block__img' src={footerData.imgsource} alt={footerData.alt} />
+          )
+        })}
+      </div>
+      <div className="footer-block__copyright">&#169; 2022 frontend</div>
+
     </footer>
   )
 }
--- myreact/src/Components/navbar/Navbar.jsx
+++ myreact/src/Components/navbar/Navbar.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
 
+
+// const menuList = 'menu__menu-list';
+const setActive = ({ isActive }) =>(isActive ? "menu__menu-list_active" : "menu__menu-list");
+
 function Navbar() {
   return (
     <header className='header-block'>
         <div className='header-block__logo'>Portfolio</div>
         <div className='header-block__menu'>
-            <NavLink to='/' className='menu__menu-list'>Home</NavLink>
-            <NavLink to='/projects' className='menu__menu-list'>Projects</NavLink>
-            <NavLink to='/contacts' className='menu__menu-list'>Contacts</NavLink>
+            <NavLink to='/' className={setActive} >Home</NavLink>
+            <NavLink to='/projects' className={setActive} >Projects</NavLink>
+            <NavLink to='/contacts' className={setActive} >Contacts</NavLink>
             <div className='menu__menu-list'>White/Black</div>
         </div>
     </header>
