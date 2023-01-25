da92df91ab8dbe9ba361e200f637b36793b4a85e
VitaliyM <vetalmamchur@gmail.com>
2023-01-25 14:40:45 +0000

    9

--- myreact/src/Components/btnDarkMode/BtnDarkMode.jsx
+++ myreact/src/Components/btnDarkMode/BtnDarkMode.jsx
@@ -4,23 +4,31 @@ import './btnDarkMode.css';
 
 function BtnDarkMode() {
 
-    // Устанавливаем начальное состояние darkMode = 'light'
-    // Изменять значение darkMode можно через ф-цию setDarkMode
-    const [darkMode, setDarkMode] = useState('light'); 
+  // Устанавливаем начальное состояние darkMode = 'light'
+  // Изменять значение darkMode можно через ф-цию setDarkMode
+  const [darkMode, setDarkMode] = useState('dark');
 
-    // useEffect() запускает ф-цию при первичном рендиренге (запуске) страницы,
-    // так же запускать ф-цию при изменении состояния
-    // Второй аргумент массив: если пустой ф-ция сработает один раз,
-    // можно передать состояние, ф-ция будет срабатывать при изменеии состояния
-    useEffect( ()=>{
-        console.log(`Use effect ${darkMode}`)
-    }, [darkMode] );
+  // useEffect() запускает ф-цию при первичном рендиренге (запуске) страницы,
+  // так же запускать ф-цию при изменении состояния
+  // Второй аргумент массив: если пустой ф-ция сработает один раз,
+  // можно передать состояние, ф-ция будет срабатывать при изменеии состояния
+  useEffect(() => {
+    console.log(`Use effect ${darkMode}`)
+    if (darkMode === 'dark') {
 
-    const toggleDarkMode = () => {
-        setDarkMode( (currentValue) => {
-            return currentValue === 'light' ? 'dark' : 'light';
-        } );
-    };
+      document.body.classList.remove('body--light-mode');
+
+    } else {
+      document.body.classList.add('body--light-mode');
+    }
+    
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((currentValue) => {
+      return currentValue === 'dark' ? 'light' : 'dark';
+    });
+  };
 
   return (
     <div className='btn-mode' onClick={toggleDarkMode}>White/Black</div>
