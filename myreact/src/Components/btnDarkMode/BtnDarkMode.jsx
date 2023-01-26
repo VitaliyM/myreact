b6eaef4098388f427bf22fc7eaaaa2ea7e875936
VitaliyM <vetalmamchur@gmail.com>
2023-01-26 15:03:46 +0000

    9

--- myreact/src/Components/btnDarkMode/BtnDarkMode.jsx
+++ myreact/src/Components/btnDarkMode/BtnDarkMode.jsx
@@ -6,47 +6,36 @@ function BtnDarkMode() {
 
   // Устанавливаем начальное состояние darkMode
   // Изменять значение darkMode можно через ф-цию setDarkMode
-  const [darkMode, setDarkMode] = useState('light');
-
- 
-  console.log("Dark Mode - " + darkMode);
-  console.log(`LocalStorage GET ${localStorage.getItem("darkMode", darkMode)}`);
-  // console.log("Before - " + JSON.parse(localStorage.getItem(darkMode)));
-  // console.log("After - " + localStorage.setItem('darkMode', JSON.stringify(darkMode)));
-
+  
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') || 'dark');
+  
+  console.log("darkMode - " + darkMode);
 
   // useEffect() запускает ф-цию при первичном рендиренге (запуске) страницы,
   // так же запускать ф-цию при изменении состояния
   // Второй аргумент массив: если пустой ф-ция сработает один раз,
   // можно передать состояние, ф-ция будет срабатывать при изменеии состояния
   useEffect(() => {
-    // let getTheme = JSON.parse(localStorage.getItem(darkMode));
-    // console.log(`localStorage Before - ${getTheme}`);
-    
-    // console.log(`Use state - ${darkMode}`);
-    
-    // localStorage.setItem('darkMode', JSON.stringify(darkMode));
 
-    // console.log(`localStorage After - ${getTheme}`);
+    localStorage.setItem('darkMode', darkMode);
 
-    
-    
-    if (darkMode === 'dark') {
-      document.body.classList.remove('body--light-mode');
-      document.querySelector('.header-block__logo').classList.remove('header-block__logo--light-mode');
-    } else {
+    if (darkMode === 'light') {
       document.body.classList.add('body--light-mode');
       document.querySelector('.header-block__logo').classList.add('header-block__logo--light-mode');
+    } else {
+      document.body.classList.remove('body--light-mode');
+      document.querySelector('.header-block__logo').classList.remove('header-block__logo--light-mode');
     }
-    
+
   }, [darkMode]);
 
-  
+
   const toggleDarkMode = () => {
-    setDarkMode( (currentValue) => {
+    setDarkMode((currentValue) => {
       return currentValue === 'dark' ? 'light' : 'dark';
     });
   };
+
 
 
   return (
